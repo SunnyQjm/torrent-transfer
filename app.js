88f369cd5db866a89a95e28e8160405428c7804b
MingJ <qjm253@gmail>
2018-07-07 22:52:47 +0800

    修跨域

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const cors = require('koa-cors');
+const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
 const combineMiddleWare = require('./middleware');
 const controller = require('./controllers');
