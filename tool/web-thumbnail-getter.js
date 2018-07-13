c263486455b85c27a194409dc5eef732d8704cd0
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-07-13 09:25:27 +0800

    增加获取网站的接口

--- tool/web-thumbnail-getter.js
+++ tool/web-thumbnail-getter.js
@@ -1,39 +1,35 @@
 const phantom = require('phantom');
-const thunky = require('thunky');
 
-const getPhantomClient = thunky(async cb => {
-    cb(await phantom.create())
-});
 
 function createWebThumbnails(url, savePath) {
-    return new Promise((resolve, reject) => {
-        getPhantomClient(client => {
-            client.createPage()
-                .then(async page => {
-                    try {
-                        let width = 1024;
-                        let height = 600;
-                        await page.property('viewportSize', {width: width, height: height});
-                        const status = await page.open(url);
-                        page.evaluate(function(w, h) {
-                            document.body.style.width = w + "px";
-                            document.body.style.height = h + "px";
-                        }, width, height);
-                        await page.property('clipRect', {top: 0, left: 0, width: width, height: height});
-                        if (status === 'success') {
-                            setTimeout(async ()=> {
-                                await page.render(savePath);
-                                resolve(savePath);
-                            }, 1000)
-                        } else {
-                            reject(status);
-                        }
-                    } catch (e) {
-                        reject(e);
+    return new Promise(async (resolve, reject) => {
+        const client = await phantom.create();
+        client.createPage()
+            .then(async page => {
+                try {
+                    let width = 1024;
+                    let height = 600;
+                    await page.property('viewportSize', {width: width, height: height});
+                    const status = await page.open(url);
+                    page.evaluate(function (w, h) {
+                        document.body.style.width = w + "px";
+                        document.body.style.height = h + "px";
+                    }, width, height);
+                    await page.property('clipRect', {top: 0, left: 0, width: width, height: height});
+                    if (status === 'success') {
+                        setTimeout(async () => {
+                            await page.render(savePath);
+                            resolve(savePath);
+                        }, 1000)
+                    } else {
+                        reject(status);
                     }
-                })
-                .catch(reject)
-        })
+                } catch (e) {
+                    reject(e);
+                }
+            })
+            .catch(reject);
+        client.exists();
     });
 }
 
